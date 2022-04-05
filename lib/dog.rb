a54bb4b1853f0962c263631d46e0718e8c5444e7
asher scott <asher.scott6@gmail.com>
2022-04-05 12:06:58 -0500

    commit tree fadd087e408205c110c872c096fe619c204ce80d

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
+    # setter
+    def name=(input)
+        @name = input
+    end
+    # getter
+    def name
+        @name
+    end
 
+    # setter
+    def breed=(input)
+        @breed = input
+    end
+    # getter
+    def breed
+        @breed
+    end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
 class Person
-
+    # setter
+    def name=(input)
+        @name = input
+    end
+    # getter
+    def name
+        @name
+    end
+    
+    # setter
+    def job=(input)
+        @job = input
+    end
+    # getter
+    def job
+        @job
+    end
 end
